d192b4a60c4d04856ca911608fc6c8010c0a409f
jtena <jtena@t2client.com>
2022-09-26 23:52:08 +0200

    search form finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormComponent} from './components/form/form.component';
+import {NewsListComponent} from './components/news-list/news-list.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {SpinnerComponent} from './animations/spinner/spinner.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    NewsListComponent,
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
